ea5ca2447ceb9a47a237a367bd2a034d1940e577
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:50:17 +0100

    Update exercise_13.py

--- exercises/exercise_13.py
+++ exercises/exercise_13.py
@@ -1,2 +1,11 @@
-# Your solution to Exercise 13
+number = int(input("Enter the four-digit number: "))
 
+digit1 = number // 1000
+digit2 = (number % 1000) // 100
+digit3 = (number % 100) // 10
+digit4 = number % 10
+
+if digit1 != digit2 and digit1 != digit3 and digit1 != digit4 and digit2 != digit3 and digit2 != digit4 and digit3 != digit4:
+  print(True)
+else:
+  print(False)
