45da7767b7d3371a12736c1fa04f538a8c3be6f2
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:45:19 +0100

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,10 @@
-# Your solution to Exercise 1
+alex_age = int(input("Enter Alex's age: "))
+tatyana_age = int(input("Enter Tatyana's age: "))
+
+if alex_age > tatiana_age:
+  print("Alex is the eldest.")
+elif tatiana_age > alex_age:
+  print("Tatyana is the eldest.")
+else:
+  print("Alex and Tatyana are of the same age.")
 
