cabe976fdc377eb51c654ea4613e1e74c8dbb2b5
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:56:05 +0100

    Update exercise_18.py

--- exercises/exercise_18.py
+++ exercises/exercise_18.py
@@ -1,2 +1,7 @@
-# Your solution to Exercise 18
+# Get input from the user for each decision point in the tree
+remember_name = input("Remember the name? (Yes/No): ")
+if remember_name == "Yes":
+  is_ex = input("Is it an ex? (Yes/No): ")
+  if is_ex == "Yes":
+    are_you_drunk = input("Are you drunk? (Yes/No): ")
 
