25d8eabf5e1e4589514f355dfae2a94ad3c19462
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:45:59 +0100

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,18 @@
 # Your solution to Exercise 3
+number = int(input("Enter the roulette number: "))
 
+if number == 0:
+  print("Green")
+elif number > 0 and number <= 36:
+  if number % 2 == 0:
+    if number >= 11 and number <= 18 or number >= 29 and number <= 36:
+      print("Red")
+    else:
+      print("Black")
+  else:
+    if number >= 1 and number <= 10 or number >= 19 and number <= 28:
+      print("Red")
+    else:
+      print("Black")
+else:
+  print("The bet will not play!")
