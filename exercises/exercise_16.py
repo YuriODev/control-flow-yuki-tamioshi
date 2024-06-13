88425c0ef4d744a7dd54e82f5c054e70582bab95
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 20:05:21 +0100

    Update exercise_16.py

--- exercises/exercise_16.py
+++ exercises/exercise_16.py
@@ -1,17 +1,23 @@
 day = int(input("Enter the day: "))
 month = int(input("Enter the month: "))
 year = int(input("Enter the year: "))
-days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-  days_in_month[1] = 29
 if day > 1:
-  day -= 1
-elif month > 1:
-  day = days_in_month[month - 2]
-  month -= 1
+    day -= 1
 else:
-  day = 31
-  month = 12
-  year -= 1
+    if month > 1:
+        month -= 1
+        if month == 2:
+            if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                day = 29
+            else:
+                day = 28
+        elif month in [1, 3, 5, 7, 8, 10, 12]:
+            day = 31
+        else:
+            day = 30
+    else:
+        month = 12
+        day = 31
+        year -= 1
 print(f"{day:02}.{month:02}.{year}")
 
