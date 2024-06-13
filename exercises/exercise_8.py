2ded8e31d68beb5388b41195db68e372bf3fe69b
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:49:15 +0100

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,11 @@
-# Your solution to Exercise 8
+number = int(input("Enter the three-digit number: "))
+digit = int(input("Enter the digit: "))
 
+digit1 = number // 100
+digit2 = (number % 100) // 10
+digit3 = number % 10
+
+if digit == digit1 or digit == digit2 or digit == digit3:
+  print(True)
+else:
+  print(False)
