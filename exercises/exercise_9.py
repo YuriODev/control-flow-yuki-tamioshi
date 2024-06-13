7ae50f479fb956fa71b1dbef07b6a49860ea870e
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:49:30 +0100

    Update exercise_9.py

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,15 @@
-# Your solution to Exercise 9
+number = int(input("Enter the three-digit number: "))
+
+digit1 = number // 100
+digit2 = (number % 100) // 10
+digit3 = number % 10
+
+sum_first_last = digit1 + digit3
+
+if sum_first_last > digit2:
+  print(">")
+elif sum_first_last < digit2:
+  print("<")
+else:
+  print("=")
 
