a9ab90d2582c8616431ca0f40d83d7f5fa705495
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:55:14 +0100

    Update exercise_16.py

--- exercises/exercise_16.py
+++ exercises/exercise_16.py
@@ -1,15 +1,17 @@
-x1 = int(input("Enter x1: "))
-y1 = int(input("Enter y1: "))
-x2 = int(input("Enter x2: "))
-y2 = int(input("Enter y2: "))
-
-distance_a = math.sqrt(x12 + y12)
-distance_b = math.sqrt(x22 + y22)
-
-if distance_a > distance_b:
-print("B is further from the origin.")
-elif distance_b > distance_a:
-print("A is further from the origin.")
+day = int(input("Enter the day: "))
+month = int(input("Enter the month: "))
+year = int(input("Enter the year: "))
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+  days_in_month[1] = 29
+if day > 1:
+  day -= 1
+elif month > 1:
+  day = days_in_month[month - 2]
+  month -= 1
 else:
-print("A and B are at the same distance from the origin.") remove math.sqr
+  day = 31
+  month = 12
+  year -= 1
+print(f"{day:02}.{month:02}.{year}")
 
