1f46778fa3569b4c4054a2388e11b9678d55543a
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 20:06:35 +0100

    Update exercise_17.py

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,8 +1,12 @@
-ticket = input("Enter the ticket number: ")
-sum1 = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
-sum2 = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
-if sum1 == sum2:
-  print("Happy")
+ticket = input("Enter the ticket number (6 digits): ")
+if len(ticket) == 6 and ticket.isdigit():
+    sum1 = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+    sum2 = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+    if sum1 == sum2:
+        print("Happy")
+    else:
+        print("Ordinary")
 else:
-  print("Ordinary")
+    print("Invalid ticket number. Please enter a 6-digit number.")
+
 
