544eb8cb8dd6b1279712326feb1f7fea4ecfe8a4
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:55:37 +0100

    Update exercise_17.py

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,2 +1,8 @@
-# Your solution to Exercise 17
+ticket = input("Enter the ticket number: ")
+sum1 = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+sum2 = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+if sum1 == sum2:
+  print("Happy")
+else:
+  print("Ordinary")
 
