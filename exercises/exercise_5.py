f1023a1f2a522a5a64b858631394e181ec752f3f
yuki-tamioshi <115879893+yuki-tamioshi@users.noreply.github.com>
2024-06-13 19:53:38 +0100

    Update exercise_5.py

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,15 @@
-# Your solution to Exercise 5
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+distance_a = math.sqrt(x1**2 + y1**2)
+distance_b = math.sqrt(x2**2 + y2**2)
+
+if distance_a > distance_b:
+  print("B is further from the origin.")
+elif distance_b > distance_a:
+  print("A is further from the origin.")
+else:
+  print("A and B are at the same distance from the origin.")
 
